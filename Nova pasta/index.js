58bb83950b027c7c49ca9346660b1e8e6fd8287c
carolmendelviana-beep <carolmendelviana@gmail.com>
2026-09-19 02:08:39 -0300

    Add files via upload

---
+++ Nova pasta/index.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Clan Japão</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <h1>CLAN JAPÃO</h1>
+
+    <p>Servidor Minecraft Semi-Anarquia</p>
+
+    <script src="script.js"></script>
+
+</body>
+</html>
---
+++ Nova pasta/script.js
@@ -0,0 +1,9 @@
+function copiarIP() {
+
+    const ip = "japanclan.minecraft.ceu.gg:21074";
+
+    navigator.clipboard.writeText(ip);
+
+    alert("IP copiado!");
+
+}
